a1157c0f7537a4796026ef36e09d6925d9410464
Ford Neild <fordneild@Fords-MacBook-Pro-2.local>
2019-08-13 23:31:46 -0500

    solved bug where reflection goes really far down page

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -9,10 +9,9 @@ class Landing extends React.Component {
         super(props)
         this.state={
             isVisible: false,
-            initialReflectionY: null,
             reflectionStyles: {
                 filter: "blur(2px)",
-                top: 0
+                transform: "rotate(180deg) scale(-1,1.5) translateY(0px)",
             }
         }
         this.reflectionRef = React.createRef()
@@ -33,16 +32,16 @@ class Landing extends React.Component {
 
     updateReflection = () => {
         const deltaReflectionY = window.pageYOffset;
-        console.log("deltaReflectionY",deltaReflectionY)
         const blur = this.INITIAL_BLUR+(deltaReflectionY/this.BLUR_CONSTANT)
-        const top = `${deltaReflectionY*this.REFLECTION_CONSTANT}`
+        let translateY = `${deltaReflectionY*this.REFLECTION_CONSTANT}`
         const reflectionStyles = {
             filter: `blur(${blur}px)`,
-            top: `${top}px`
+            transform: `rotate(180deg) scale(-1,1.5) translateY(${-1*translateY}px)`
         }
         this.setState({
-            reflectionStyles: reflectionStyles
-        },console.log(this.state.reflectionStyles))
+            reflectionStyles: reflectionStyles,
+            translateY: translateY
+        })
     }
         
     
@@ -54,19 +53,19 @@ class Landing extends React.Component {
             window.removeEventListener('scroll',this.updateReflection)
         }
         this.setState({
-            isVisibe: isVisible
-        },console.log("isVisible",isVisible))
+            isVisible: isVisible
+        })
     }
 
 
     
     render(){
-       console.log("in render",this.state.isVisible)
+        const {isVisible} = this.state;
         return(
             <VisibilitySensor partialVisibility={true} onChange={this.onChange}>
                 <section className="landing">
-                <img src={LandingImage}/>
-                    <div className="landing__intro">
+                <img className={isVisible? "fixed": null} src={LandingImage}/>
+                    <div className={`landing__intro ${!isVisible? "gone": ""}`}>
                         <div className="text">
                             <h4>Meet Me</h4>
                             <h1>Ford Neild</h1>
