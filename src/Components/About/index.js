0f1e2e823259f35112b17611ce00581d4fcb0deb
Ford Neild <fordneild@Fords-MacBook-Pro-2.local>
2019-12-20 16:05:41 -0500

    Added <br> tag in bio

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -14,7 +14,7 @@ const About = () => {
             My name is Ford Neild. I am a Junior at Johns Hopkins University studying computer science.
             <br /><br />
             Software development is my passion and I often find myself getting lost in my work for hours. I enjoy working with similarly enthusatic and motivated people. During my summer and winter breaks, I have developed commerical websites with React and jQuery. Web Development is extremely gratifing to me becuase of the vast and immediate impact it can have on the world. In my academic studies, I enjoy using Java, C/C++ and Python. Although I love web development, my area of concentration for my CS degree is machine learning. In addition to taking the full ML courseload offered at Hopkins, I have completed several personal projects with Tensor Flow.
-            <br></br>            
+            <br /><br />            
             Currently, I am taking classes on campus and will return to my internship at Way2B1 for my month long winter break in January. I am also seeking an internship for Summer 2020 that will expand upon my skills in software development and build real-world experience as part of a programming team.
           </p>
           {/* <div className="test">
