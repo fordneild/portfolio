5c48425ffa3532982b6e1a21d0b5c6d08e32b650
Ford Neild <hanfordn35@gmail.com>
2020-01-15 16:51:38 -0800

    Added profile photo, revised copy and updated resume

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.scss";
+import ProfilePhoto from '../../assets/portfolio_image.jpg'
 
 const About = () => {
   return (
@@ -8,20 +9,39 @@ const About = () => {
       <div className="sheered">
         <div className="reverse-sheered">
           <h1>about</h1>
-          <p>
-            Hi.
-            <br /><br />
-            My name is Ford Neild. I am a Junior at Johns Hopkins University studying computer science.
-            <br /><br />
-            Software development is my passion and I often find myself getting lost in my work for hours. I enjoy working with similarly enthusatic and motivated people. During my summer and winter breaks, I have developed commerical websites with React and jQuery. Web Development is extremely gratifing to me becuase of the vast and immediate impact it can have on the world. In my academic studies, I enjoy using Java, C/C++ and Python. Although I love web development, my area of concentration for my CS degree is machine learning. In addition to taking the full ML courseload offered at Hopkins, I have completed several personal projects with Tensor Flow.
-            <br /><br />            
-            Currently, I am taking classes on campus and will return to my internship at Way2B1 for my month long winter break in January. I am also seeking an internship for Summer 2020 that will expand upon my skills in software development and build real-world experience as part of a programming team.
-          </p>
-          {/* <div className="test">
+          <div className="about__content">
+            <div className="about__content__column">
+              <p>
+                I'm Ford, a Junior at Johns Hopkins University, studying computer
+                science. I have a passion for thinking of and carrying out
+                full-stack features from start to finish. I love every stage in the process, from brainstorming to unit testing.
+              </p>
+              <div className="profile-picture--container">            
+              <img src={ProfilePhoto} alt=""/>
+              </div>
+            </div>
+            <div className="about__content__column">
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Web Development is rewarding to me because of the vast and immediate
+              impact it can have on people. I especially enjoy creating
+              applications that harness the power of machine learning to provide
+              a smarter UX. In addition to taking the full ML courseload offered
+              at Hopkins, I have completed several personal projects with Tensor
+              Flow. 
+              <br />
+              <br /> 
+              Currently, I am working at Way2B1 in San Francisco over my
+              month-long winter break. I am also seeking an internship for
+              Summer 2020 that will expand upon my skills in software
+              development and build real-world experience as part of a
+              programming team.
             </p>
-          </div> */}
+            <br />
+              <br /> 
+            </div>
+            
+            
+          </div>
         </div>
       </div>
     </section>
