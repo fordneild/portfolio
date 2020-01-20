5aeceaae02a0e1444d0e9008785d44e3070b198d
Ford Neild <hanfordn35@gmail.com>
2020-01-20 14:31:36 -0800

    Added base of slider need to make transition

--- src/Components/Slider/index.js
+++ src/Components/Slider/index.js
@@ -1,22 +1,71 @@
-import React from 'react'
-import './Slider.scss'
+import React from "react";
+import "./Slider.scss";
 
 class Slider extends React.Component {
-    
-    
-    render(){
-        return(
-            <div className="slider-container">
-                <div className="slide"></div>
-                <div className="slide"></div>
-                <div className="slide"></div>
-                <div className="slide"></div>
-                <div className="slide"></div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    };
+    this.activeSlideRef = React.createRef();
+    this.sliderRef = React.createRef();
+  }
 
-            </div>
-        )
-        
+  updateSliderPosition = () => {
+    //put the middle of the current ref in the center of the slider
+    const { activeSlideRef } = this;
+    if (activeSlideRef) {
+      const curSlide = activeSlideRef.current;
+      if (curSlide) {
+        const midOfSlide = curSlide.clientWidth / 2 + curSlide.offsetLeft;
+        console.log("updating slider position",midOfSlide)
+        this.setState({ offtsetLeft: midOfSlide });
+      }
     }
+  };
+
+  setActiveSlide = index => {
+    console.log("Click!", index);
+    this.setState({ activeIndex: index });
+    this.updateSliderPosition();
+  };
+  sliderData = [
+    "lightgreen",
+    "lightblue",
+    "lightred",
+    "lightyellow",
+    "lightpurple",
+    "lightorange"
+  ];
+
+  render() {
+    const { state, activeSlideRef, sliderRef } = this;
+    const { activeIndex, offsetLeft = 0 } = state;
+    return (
+      <div ref={sliderRef} className="slider-window">
+        <div
+          style={{
+            transform: `translateX(${offsetLeft}px)`
+          }}
+          className="slider-track"
+        >
+          {this.sliderData.map((slideData, index) => {
+            return (
+              <div
+                onClick={() => this.setActiveSlide(index)}
+                key={index}
+                ref={activeIndex === index ? activeSlideRef : null}
+                style={{
+                  backgroundColor: slideData
+                }}
+                className={`slide ${activeIndex === index ? "active" : ""}`}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Slider
+export default Slider;
